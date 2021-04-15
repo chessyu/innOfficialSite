5cabdd01768fe7e8c251ce498cd4bf556292ee73
chessyu <472734301@qq.com>
2021-04-15 23:27:25 +0800

    新增图片
    新增 发现研究所页面

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,10 +1,13 @@
 $(function(){
+    //店铺地址赋值
+    $('.storeMap').attr({"href":"https://www.amap.com/search?query=%E5%85%A8%E5%BF%83%E6%97%85%E9%A6%86&city=440100&geoobj=113.337388|23.191003|113.341214|23.193152&zoom=19.1","target":'_blank'})
+
     //表头菜单固定
     $(window).on('scroll', function () {
 		if ($(this).scrollTop() > 250) {
-			$('.site-content').addClass('sticky');
+			$('.custom .site-content').addClass('sticky');
 		} else {
-			$('.site-content').removeClass('sticky');
+			$('.custom .site-content').removeClass('sticky');
 		}
 	});
 
@@ -18,4 +21,24 @@ $(function(){
 		parallax: true,
         autoplay: true,
 	});
+
+    // 图片轮播
+    var testimonialCarousel = new Swiper('.swiper-slide-box', {
+        loop: true,
+        speed: 750,
+        spaceBetween: 30,
+        slidesPerView: 1,
+		effect: 'slide',
+		parallax: true,
+        lazyLoading:true,
+        navigation: {
+            nextEl: '.home-slider-next',
+            prevEl: '.home-slider-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: 'true',
+		},
+    });
 })
